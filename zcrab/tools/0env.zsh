dd93320eed489fae41fbf41a8df4339f39a758a0
Pcrab <pcrab@pcrab.xyz>
2021-07-08 18:26:19 +0800

    change environment variables

--- zcrab/tools/0env.zsh
+++ zcrab/tools/0env.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+
 export BW_SESSION="DkuXtaHMO1qrhWyWSI+B84aL29bjwc/k5GTKR/hqn+zMvOSX+ylEqShvPVvqIBxgtYzQk7r6U1ZgWIocm+JtNw=="
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
@@ -11,14 +12,13 @@ export CCACHE_DIR="$XDG_CACHE_HOME"/ccache
 export CPATH=/usr/local/opt/openssl/include
 export LIBRARY_PATH=/usr/local/opt/openssl/lib
 # QT
-export LDFLAGS="-L/usr/local/opt/qt/lib"
-export CPPFLAGS="-I/usr/local/opt/qt/include"
-export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
+export LDFLAGS="-L/usr/local/opt/qt/lib -L/usr/local/opt/openblas/lib"
+export CPPFLAGS="-I/usr/local/opt/qt/include -I/usr/local/opt/openblas/include"
+export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig /usr/local/opt/openblas/lib/pkgconfig"
 
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
 export MACHINE_STORAGE_PATH="$XDG_DATA_HOME"/docker-machine
 export ELECTRUMDIR="$XDG_DATA_HOME/electrum"
-export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 export GTK_RC_FILES="$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
 export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
 export KDEHOME="$XDG_CONFIG_HOME"/kde
@@ -34,13 +34,18 @@ export RXVT_SOCKET="$XDG_RUNTIME_DIR"/urxvtd
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
 export WINEPREFIX="$XDG_DATA_HOME"/wineprefixes/default
 
-# export GOROOT="/usr/lib/go"
-# export GOPATH="$HOME/.local/lib/golib:$HOME/doc/dev/go"
 export GOPATH=$HOME/.local/go
+export GOPROXY="https://goproxy.cn"
+export GO111MODULE="auto"
+
+# export RUSTUP_HOME="$XDG_CONFIG_HOME/rustup"
+# export CARGO_HOME="$XDG_CONFIG_HOME/cargo"
+
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
-# export https_proxy=http://127.0.0.1:7890
-# export http_proxy=http://127.0.0.1:7890
-# export all_proxy=socks5://127.0.0.1:7891
+export https_proxy=http://127.0.0.1:7890
+export http_proxy=http://127.0.0.1:7890
 
-export PATH="$HOME/node_modules/yarn/bin:/usr/local/opt/openjdk/bin:/usr/local/Cellar/llvm/11.0.0/bin:$GOPATH/bin:$PATH"
+export EDITOR="/usr/local/bin/nvim"
+
+export PATH="$HOME/.cargo/bin/:$HOME/node_modules/yarn/bin:/usr/local/go/bin/:/usr/local/opt/openjdk/bin:/usr/local/Cellar/llvm/11.0.0/bin:$GOPATH/bin:/usr/local/Cellar/r/4.0.3/bin/:$PATH"
