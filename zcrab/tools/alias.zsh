17fa2872b4d43e4536a327ad16ad930c0b473576
Pcrab <pcrab@pcrab.xyz>
2021-07-08 18:24:47 +0800

    change aliases

--- zcrab/tools/alias.zsh
+++ zcrab/tools/alias.zsh
@@ -2,26 +2,25 @@
 
 # ls
 alias \
-    ls="exa" \
-    la="exa -lahg" \
-    l="exa -lahg" \
-    ll="exa -lhg"
+    ls="lsd" \
+    l="lsd -lah" \
+    ll="lsd -lh"
 
 # shortcut
 alias \
     c="clear" \
-    r="reset" \
-    sdn="doas shutdown -h now"\
+    re="reset" \
+    sdn="doas shutdown -h now" \
     gcpp="g++ -Wall -std=c++14"
 
 # rename
 alias \
     top="htop -t" \
     grep="grep --color=auto" \
-    pandocn='pandoc --pdf-engine=xelatex -V CJKmainfont="Source Han Sans SC"' \
-    doas="doas --" \
-    vim="nvim" \
-    wget="wget --hsts-file='$XDG_CACHE_HOME/wget-hsts'"
+    md2pdf='pandoc --pdf-engine=xelatex --template=pm-template.tex' \
+    vim="/usr/local/bin/vim" \
+    wget="wget --hsts-file='$XDG_CACHE_HOME/wget-hsts'" \
+    cat="bat" # vim="nvim" \
 
 # apps
 alias \
@@ -37,11 +36,24 @@ alias \
 
 # for secure
 alias \
-    rm="rm -Iv" \
+    rm="rm -iv" \
     cp="cp -iv" \
     mv="mv -iv"
 # go
 alias \
-    cg="cd ~/.local/go/src/git/pcrab/" \
-    cv="cd $HOME/.vim/"\
-    cn="cd $XDG_CONFIG_HOME/nvim/"\
+    cg="cd $GOPATH/src/pcrab.xyz/pcrab/" \
+    cv="cd $HOME/.vim/" \
+    cn="cd $XDG_CONFIG_HOME/nvim/" \
+    cz="cd $XDG_CONFIG_HOME/zsh/" \
+    cb="cd $XDG_DATA_HOME/hugo-blog/" \
+    ct="cd $XDG_DATA_HOME/hugo-blog/themes/crabquark/"
+
+# gpg
+alias \
+    cs="gpg --card-status"
+
+# tmux
+alias \
+    tl="tmux ls" \
+    tn="tmux new -s" \
+    ta="tmux a -t"
